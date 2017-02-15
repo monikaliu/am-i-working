41e99616b95888d433f64d772936354ea6ce2786
Monika <monika.liutk@gmail.com>
2017-02-15 11:35:32 +0200

    style clean up

--- work.js
+++ work.js
@@ -1,23 +1,25 @@
-document.getElementsByClassName("btn")[0].addEventListener("click", checkActivity, false);
+(function() {
+	document.getElementsByClassName("button_check")[0].addEventListener("click", checkActivity, false);
 
-function checkActivity() {
-	hideElement(document.querySelector(".btn"));
-	showElement(document.querySelector(".spinner"));
-	setTimeout(showResult, 1500);
-}
+	function checkActivity() {
+		hideElement(document.querySelector(".button_check"));
+		showElement(document.querySelector(".spinner"));
+		setTimeout(showResult, 1500);
+	}
 
-function showResult(){
-	hideElement(document.querySelector(".spinner"));
-	showElement(document.querySelector(".answer"));
-}
+	function showResult(){
+		hideElement(document.querySelector(".spinner"));
+		showElement(document.querySelector(".answer"));
+	}
 
-function hideElement(el) {
-	el.classList.remove("show");
-	el.classList.add("hide"); 
-}
+	function hideElement(element) {
+		element.classList.remove("show");
+		element.classList.add("hide"); 
+	}
 
-function showElement(el) {
-	el.classList.remove("hide");
-	el.classList.add("show"); 
-}
+	function showElement(element) {
+		element.classList.remove("hide");
+	}
+
+})();
 
